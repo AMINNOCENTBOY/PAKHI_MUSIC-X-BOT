ebe3cb8e363b980f3681cfe286fa2bc3d99efbb3
AMINNOCENTBOY <155085235+AMINNOCENTBOY@users.noreply.github.com>
2024-06-25 23:20:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,6 +23,7 @@ EVALOP = list(map(int, getenv("EVALOP", "7001982096").split()))
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 

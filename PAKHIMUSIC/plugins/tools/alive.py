7818b322fde84dbbdad759c69bf05d11dc71ab9a
AMINNOCENTBOY <155085235+AMINNOCENTBOY@users.noreply.github.com>
2024-06-26 10:40:10 +0530

    Update alive.py

--- PAKHIMUSIC/plugins/tools/alive.py
+++ PAKHIMUSIC/plugins/tools/alive.py
@@ -11,15 +11,9 @@ async def start(client: Client, message: Message):
     await message.reply_video(
         video=f"https://graph.org/file/e999c40cb700e7c684b75.mp4",
         caption=_["ping_1"].format(app.mention),
-        )
-    pytgping = await Anony.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 1000
-    await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
-       
-              [
+        reply_markup=InlineKeyboardMarkup(
+            [
+               [
             InlineKeyboardButton(
                 text="⎯꯭‌✭𝆺꯭𝅥𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁✭", url=f"https://t.me/InnocentIdkaaa"
             ),
@@ -37,6 +31,6 @@ async def start(client: Client, message: Message):
                         "⛧ᴄʟᴏsᴇ⛧", callback_data="close"
                     )
                 ],
-            ] 
+            ]
         )
     )

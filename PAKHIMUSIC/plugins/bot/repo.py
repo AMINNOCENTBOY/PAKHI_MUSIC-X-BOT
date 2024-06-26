80ba1a54eb24129cc506b122d8bdbce6f7c897a9
AMINNOCENTBOY <155085235+AMINNOCENTBOY@users.noreply.github.com>
2024-06-26 23:05:48 +0530

    Update repo.py

--- PAKHIMUSIC/plugins/bot/repo.py
+++ PAKHIMUSIC/plugins/bot/repo.py
@@ -14,15 +14,14 @@ async def start(_, msg):
     buttons = [
      
             [ 
-            InlineKeyboardButton(""✭ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ✭", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            InlineKeyboardButton("✭ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ✭", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
             ],
      
             [
              InlineKeyboardButton("⎯꯭‌✭𝆺꯭𝅥ᴿᴱᴾᴼ .𓈀✔", url="https://t.me/friendship_forever_group143"),
              InlineKeyboardButton("⎯꯭‌✭𝆺꯭𝅥𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁✭", url="https://t.me/InnocentIdkaaa"),
              ],
-     
-         
+    
        ]
     
     reply_markup = InlineKeyboardMarkup(buttons)

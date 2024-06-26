6c7a0b1ce8746b37dd7b403fa4a51a83f288b259
AMINNOCENTBOY <155085235+AMINNOCENTBOY@users.noreply.github.com>
2024-06-26 17:03:24 +0530

    Update repo.py

--- PAKHIMUSIC/plugins/bot/repo.py
+++ PAKHIMUSIC/plugins/bot/repo.py
@@ -53,9 +53,8 @@ async def start(_, msg):
        ]
     
     reply_markup = InlineKeyboardMarkup(buttons)
-    await msg.reply__media(
-        InputMediaVideo("https://telegra.ph/file/3473a79595e79f3a494c0.mp4"),
-        reply_markup=InlineKeyboardMarkup(
-             
-       caption=start_txt,
+    await msg.reply_media(
+        media="https://telegra.ph/file/3473a79595e79f3a494c0.mp4",
+        caption=start_txt,
+        reply_markup=reply_markup
     )
